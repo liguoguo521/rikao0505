7f86c0dafbe6a99b7a5e78f25394d5e02117e2c1
liguodong <1173179369@qq.com>
2018-12-06 10:20:52 +0800

    dev开发环境完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,32 @@
-var gulp=require('gulp');
-var sass=require('gulp-sass');
-var mincss=require('gulp-clean-css');
-var server=require('gulp-webserver');
+//内置模块
+var gulp=require("gulp");
+var sass=require("gulp-sass");
+var minCss=require('gulp-clean-css');
+var server =require('gulp-webserver');
+var url=require('url');
+var fs=require('fs');
+var path=require('path');
 
-gulp.task('scss',function(){
+//编译scss
+gulp.task ("scss",function() {
   return gulp.src('./src/scss/*.scss')
   .pipe(sass())
-  .pipe(mincss())
+  .pipe(minCss())
   .pipe(gulp.dest('./src/styles'))
 })
 
+//监听
+gulp.task('watch',function(){
+  return gulp.watch('./src/scss/',gulp.series('scss'))
+})
 
+//其服务
+gulp.task('server',function(){
+  return gulp.src('src')
+  .pipe(server({
+    port:8888,
+    open:true,
+  }))
+})
+
+gulp.task('dev',gulp.series('scss','server','watch'));
